f3975906b9570f4aa9739febd5f6bdfe4a273ca3
JeremieOverGs <104275065+JeremieOverGs@users.noreply.github.com>
2022-04-23 22:59:41 +0200

    Add files via upload

---
+++ ARES Transpiler/ARES snippets/helloworld.ares.cpp
@@ -0,0 +1,13 @@
+// using the ARES language 2022, under GPL3 license.
+// Transpile date: 2022/59/23 10:59:09
+
+// ARES - Headers
+#include "ARES.h"
+
+// ARES - Overhead prototypes
+int main();
+
+// ARES - Program
+int main() {
+ARES::Print( "Hello, World!");
+};
